3c6266a983124ac4213b13f415fb39428544abc0
Tomachi <8929706+book000@users.noreply.github.com>
2022-08-16 00:44:52 +0900

    fix: image name

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 docker network create chatwatcher-network
 # docker build -t chatwatcher-recognizer recognizer
-docker pull ghcr.io/jaoafa/ChatWatcher-recognizer
+docker pull ghcr.io/jaoafa/chatwatcher-recognizer
 
 docker-compose up --build -d
