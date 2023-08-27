add284d4ea27d3d72e52bea05ed571cc2c8e2a89
yuuaHP <identity@yuua.dev>
2023-08-27 14:31:55 +0900

    fix: unintended disconnection & code cleanup (#55)
    Co-authored-by: Tomachi <8929706+book000@users.noreply.github.com>

--- src/main/java/com/jaoafa/chatwatcher/event/AutoJoin.java
+++ src/main/java/com/jaoafa/chatwatcher/event/AutoJoin.java
@@ -1,6 +1,7 @@
 package com.jaoafa.chatwatcher.event;
 
 import com.jaoafa.chatwatcher.lib.Utils;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -8,14 +9,16 @@ import org.jetbrains.annotations.NotNull;
 public class AutoJoin extends ListenerAdapter {
     @Override
     public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
-        if (event.getChannelJoined() == null) return; // 参加と移動以外は除外
-        if (event.getMember().getUser().isBot()) {
-            return;
-        }
-        if (event.getGuild().getSelfMember().getVoiceState() != null &&
-                event.getGuild().getSelfMember().getVoiceState().getChannel() != null) {
-            return; // 自身がいずれかのVCに参加している
-        }
+        if (event.getMember().getUser().isBot()) return;
+
+        // 参加と移動以外は除外
+        if (event.getChannelJoined() == null) return;
+
+        GuildVoiceState selfVoiceState = event.getGuild().getSelfMember().getVoiceState();
+
+        // 自身がいずれかのVCに参加している
+        if (selfVoiceState != null && selfVoiceState.getChannel() != null) return;
+
         Utils.connectVoiceChannel(event.getGuild(), event.getChannelJoined());
     }
 }
